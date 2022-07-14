5866d6e891b8ff8bbf1d821a12fd64dbd7752d13
Václav Volhejn <8401624+vvolhejn@users.noreply.github.com>
2022-07-14 17:32:31 +0200

    Use ~/.local and not ~/local for fish

--- rootless_fish.sh
+++ rootless_fish.sh
@@ -2,7 +2,7 @@
 
 # https://gist.github.com/masih/10277869
 # Script for installing Fish Shell on systems without root access.
-# Fish Shell will be installed in $HOME/local/bin.
+# Fish Shell will be installed in $HOME/.local/bin.
 # It's assumed that wget and a C/C++ compiler are installed.
  
 # exit on error
@@ -11,7 +11,7 @@ set -e
 FISH_SHELL_VERSION=3.2.2
  
 # create our directories
-mkdir -p $HOME/local $HOME/fish_shell_tmp
+mkdir -p $HOME/.local $HOME/fish_shell_tmp
 cd $HOME/fish_shell_tmp
  
 # download source files for Fish Shell
@@ -22,12 +22,12 @@ wget http://github.com/fish-shell/fish-shell/releases/download/${FISH_SHELL_VERS
 tar xvf fish-${FISH_SHELL_VERSION}.tar.xz
 cd fish-${FISH_SHELL_VERSION}
 mkdir build; cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/local
-# ./configure --prefix=$HOME/local --disable-shared
+cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/.local
+# ./configure --prefix=$HOME/.local --disable-shared
 make
 make install
 
 # Add the following to ~/.bashrc:
 # if [[ $- = *i* ]]; then
-#   exec ~/local/bin/fish
+#   exec ~/.local/bin/fish
 # fi

941a273af8f7bb316f3bd21522b8718a3495cd25
Václav Volhejn <vv@lakera.ai>
2024-09-04 11:22:19 +0200

    Poetry aliases

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -1,1 +1,5 @@
-# Empty so far.
+# Abbreviations expand a short command to a longer one.
+# This is different from an alias because the expansion is done
+# when the command is typed, not when it is run.
+abbr --add prp poetry run python
+abbr --add pr poetry run
